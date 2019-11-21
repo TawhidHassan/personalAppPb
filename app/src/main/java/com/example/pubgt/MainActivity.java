ee51f3c742bdfa9bb53198a19b85877b1cb16419
sifat hassan <sifat27.sh22@gmail.com>
2019-11-21 17:40:50 +0600

    homePage Baner slider and coustomize code make it clear user friendly

--- app/src/main/java/com/example/pubgt/MainActivity.java
+++ app/src/main/java/com/example/pubgt/MainActivity.java
@@ -25,30 +25,47 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.Menu;
 import android.widget.FrameLayout;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration mAppBarConfiguration;
+    //top app bar
     private BottomAppBar bottomAppBar;
+    //top app bar
+
+    //bottom app bar\
+    private AppBarConfiguration mAppBarConfiguration;
+    //bottom app bar
+
+    //fab icon
     private FloatingActionButton fab;
+    //fab icon
     private boolean isFabTapped = false;
+    //main frame layout
     FrameLayout frameLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //tool bar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        //bottom app bar
         bottomAppBar = findViewById(R.id.bottomAppBar);
-
-
+        //fab btn
          fab = findViewById(R.id.fab);
+         //frame layout
         frameLayout = findViewById(R.id.main_frameLayoutId);
 
+
+//-------------------------------------------------------------------------------------------
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -61,12 +78,14 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+//------navigation drawer aprt end----------------------------------------------------------------------------------------
 
         //bottom nav bar
-
         handleFab();
         handleOnClickListeners();
-
+//This code snippet adds specified (R.menu.bottom_menu) menu to BottomAppBar
+        bottomAppBar.replaceMenu(R.menu.bottom_menu);
+        //bottom nav bar
 
     }
 
@@ -75,7 +94,6 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
-
         return true;
     }
 
--- app/src/main/java/com/example/pubgt/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/pubgt/ui/home/HomeFragment.java
@@ -1,7 +1,10 @@
 package com.example.pubgt.ui.home;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -11,25 +14,134 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.pubgt.R;
+import com.example.pubgt.SliderAdapter;
+import com.example.pubgt.SliderModel;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
 
+    //slider banner
+    private ViewPager bannerSliderViewPager;
+    private List<SliderModel> sliderModelList;
+    private int cureentSlider=2;
+    private Timer timer;
+    final private long Delay_time=3000;
+    final private long Period_time=3000;
+    //slider banner
+    @SuppressLint("ClickableViewAccessibility")
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-//        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
+        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        //baner slider viewPager
+        bannerSliderViewPager = view.findViewById(R.id.banner_sliderViewPagerId);
+
+        //top banner slider
+
+        sliderModelList=new ArrayList<SliderModel>();
+
+        sliderModelList.add(new SliderModel(R.drawable.banner));
+        sliderModelList.add(new SliderModel(R.drawable.banner));
+
+        sliderModelList.add(new SliderModel(R.drawable.banner));
+        sliderModelList.add(new SliderModel(R.drawable.banner));
+        sliderModelList.add(new SliderModel(R.drawable.banner));
+        sliderModelList.add(new SliderModel(R.drawable.banner));
+        sliderModelList.add(new SliderModel(R.drawable.banner));
+        sliderModelList.add(new SliderModel(R.drawable.photoeight));
+        sliderModelList.add(new SliderModel(R.drawable.photoeight));
+
+        sliderModelList.add(new SliderModel(R.drawable.banner));
+        sliderModelList.add(new SliderModel(R.drawable.banner));
+
+        SliderAdapter sliderAdapter=new SliderAdapter(sliderModelList);
+        bannerSliderViewPager.setAdapter(sliderAdapter);
+        bannerSliderViewPager.setClipToPadding(false);
+        bannerSliderViewPager.setPageMargin(20);
+
+        //listener
+        bannerSliderViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                cureentSlider=position;
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                if (state==ViewPager.SCROLL_STATE_IDLE)
+                {
+                    pageLooper(sliderModelList);
+                }
             }
         });
-        return root;
+        startBannerSlideShow();
+
+        bannerSliderViewPager.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                pageLooper(sliderModelList);
+                stopBannerSlide();
+                if (event.getAction()==MotionEvent.ACTION_UP)
+                {
+                    startBannerSlideShow();
+                }
+                return false;
+            }
+        });
+        //top banner slider
+
+        return view;
+    }
+
+    //for slider banner auto scrool
+    private void pageLooper(List<SliderModel> sliderModelList)
+    {
+        if (cureentSlider==sliderModelList.size()-2){
+            cureentSlider=2;
+            bannerSliderViewPager.setCurrentItem(cureentSlider,false);
+        }
+        if (cureentSlider==1){
+            cureentSlider=sliderModelList.size()-3;
+            bannerSliderViewPager.setCurrentItem(cureentSlider,false);
+        }
+    }
+    private void startBannerSlideShow(){
+        final Handler handler=new Handler();
+        final Runnable update=new Runnable() {
+            @Override
+            public void run() {
+                if (cureentSlider>=sliderModelList.size())
+                {
+                    cureentSlider=-1;
+                }
+                bannerSliderViewPager.setCurrentItem(cureentSlider++,true);
+            }
+        };
+        timer=new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(update);
+            }
+        },Delay_time,Period_time);
+    }
+    private void stopBannerSlide()
+    {
+        timer.cancel();
     }
 }
