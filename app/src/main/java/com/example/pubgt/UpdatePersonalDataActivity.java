fad57da2627bbdece44717801597134c545e6b3b
sifat hassan <sifat27.sh22@gmail.com>
2019-11-28 17:12:17 +0600

    change changePassword Activity background Color

---
+++ app/src/main/java/com/example/pubgt/UpdatePersonalDataActivity.java
@@ -0,0 +1,14 @@
+package com.example.pubgt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpdatePersonalDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_personal_data);
+    }
+}
