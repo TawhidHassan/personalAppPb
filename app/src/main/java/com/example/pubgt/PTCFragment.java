8fe8b6a6571abe38cbc82c6dac67a943abc0036e
sifat hassan <sifat27.sh22@gmail.com>
2019-11-28 19:50:16 +0600

    design ptc fragment

--- app/src/main/java/com/example/pubgt/PTCFragment.java
+++ app/src/main/java/com/example/pubgt/PTCFragment.java
@@ -5,27 +5,47 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PTCFragment extends Fragment {
 
+    // Inflate the layout for this fragment
+    private TextView txtProgress;
+    private ProgressBar progressBar;
+    private int pStatus = 0;
+    private Handler handler = new Handler();
+    // Inflate the layout for this fragment
 
     public PTCFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ptc, container, false);
+        View view = inflater.inflate(R.layout.fragment_ptc, container, false);
+
+        //inite item for ptc status circle progress bar
+        txtProgress = (TextView) view.findViewById(R.id.txtProgress);
+        progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
+
+
+        progressBar.setProgress(80);
+        txtProgress.setText(""+8);
+
+
+        // Inflate the layout for this fragment
+
+        return view;
     }
 
 }
