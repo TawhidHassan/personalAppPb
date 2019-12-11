f2ccf70f0e729ff8e441144d9079ec0ef1d1a46b
sifat hassan <sifat27.sh22@gmail.com>
2019-12-11 19:50:03 +0600

    wc fragment exchange part we coutomize exchange option make pop up dialog

--- app/src/main/java/com/example/pubgt/ExchangeTkFragment.java
+++ app/src/main/java/com/example/pubgt/ExchangeTkFragment.java
@@ -8,6 +8,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 
 /**
@@ -15,17 +18,41 @@ import android.view.ViewGroup;
  */
 public class ExchangeTkFragment extends Fragment {
 
-
+    private BottomSheetBehavior bottomSheetBehavior;
+    Button sheetCloseBooton;
+    Button sheetButton;
     public ExchangeTkFragment() {
         // Required empty public constructor
     }
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_exchange_tk, container, false);
+        View view= inflater.inflate(R.layout.fragment_exchange_tk, container, false);
+
+        //weekUp to bootom layout
+        View bottomSheet = view.findViewById(R.id.bottom_sheet);
+        bottomSheetBehavior =BottomSheetBehavior.from(bottomSheet);
+
+         sheetButton = view.findViewById(R.id.exchangeBottonId);
+         sheetCloseBooton = view.findViewById(R.id.sheetCloseBootonId);
+        sheetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+            }
+        });
+        sheetCloseBooton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bottomSheetBehavior.setHideable(true);//Important to add
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+            }
+        });
+        //weekUp to bootom layout
+
+
+        return view;
     }
 
 }
--- app/src/main/java/com/example/pubgt/ui/send/SendFragment.java
+++ app/src/main/java/com/example/pubgt/ui/send/SendFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -13,23 +14,41 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.pubgt.R;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class SendFragment extends Fragment {
 
-    private SendViewModel sendViewModel;
+    private BottomSheetBehavior bottomSheetBehavior;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        sendViewModel =
-                ViewModelProviders.of(this).get(SendViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_send, container, false);
-        final TextView textView = root.findViewById(R.id.text_send);
-        sendViewModel.getText().observe(this, new Observer<String>() {
+
+        View view = inflater.inflate(R.layout.fragment_send, container, false);
+        FloatingActionButton fab = view.findViewById(R.id.fab);
+
+        View bottomSheet = view.findViewById(R.id.bottom_sheet);
+        bottomSheetBehavior =BottomSheetBehavior.from(bottomSheet);
+
+        Button button = view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
             }
         });
-        return root;
+
+
+
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "This is a Snackbar", Snackbar.LENGTH_LONG).show();
+            }
+        });
+
+        return view;
     }
 }
